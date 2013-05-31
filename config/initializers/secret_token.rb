67b00bc1237b656e84c8eebcfc37025d2e0b4ce3
Karthikeyan A K <mindaslab@gmail.com>
2013-05-31 08:02:27 +0530

    Kalvi started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kalvi::Application.config.secret_token = '62fc5ec63e9f4469c0b2eff2f95edf8c5d2825526f3d746e14bce3c4d3dd2b02223ae5cd438d62c5e49545e0648a2ab1f93dc4c80c7ae444402183de141c5fc9'
